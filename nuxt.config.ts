edcbf87df981987fdb7737d5aee1de0cffe822a1
madaoHas <ginsan78963@mail.ru>
2023-09-07 17:54:24 +0300

    add netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  // modules: [
-  //   // Simple usage
-  //   'nuxt-buefy',
-  // ]
-})
+// export default defineNuxtConfig({
+//   devtools: { enabled: true },
+//   // modules: [
+//   //   // Simple usage
+//   //   'nuxt-buefy',
+//   // ]
+// })
+
+export default {
+  generate: {
+    fallback: true
+  }
+}
